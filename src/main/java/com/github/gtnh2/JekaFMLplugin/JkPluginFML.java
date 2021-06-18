7bac56726c64fdad3f46d1f75dc9c24050d33fb3
SirFell <vitaliilesik@gmail.com>
2021-06-18 22:05:51 +0300

    Generate Jeka skeleton and create main plugin class

---
+++ src/main/java/com/github/gtnh2/JekaFMLplugin/JkPluginFML.java
@@ -0,0 +1,13 @@
+package com.github.gtnh2.JekaFMLplugin;
+
+import dev.jeka.core.tool.JkClass;
+import dev.jeka.core.tool.JkDoc;
+import dev.jeka.core.tool.JkPlugin;
+
+@JkDoc("Minecraft mod development framework for Forge and FML for jeka build system")
+public class JkPluginFML extends JkPlugin {
+
+    protected JkPluginFML(JkClass jkClass) {
+        super(jkClass);
+    }
+}
